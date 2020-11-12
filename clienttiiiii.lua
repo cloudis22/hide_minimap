58241702718619aba8953b53292dccfde6cec3b0
cloudis22 <68263921+cloudis22@users.noreply.github.com>
2020-11-12 17:56:18 +0200

    Add files via upload

---
+++ clienttiiiii.lua
@@ -0,0 +1,33 @@
+-----------------------------CLOUDIS RADAR
+-- BY CLOUDIS
+-- BY CLOUDIS
+-- BY CLOUDIS
+-- BY CLOUDIS
+kartta = false
+ESX = nil
+
+Citizen.CreateThread(function()
+    while true do
+        while not kartta do
+            if IsControlJustPressed(1, 56) then --F9
+                kartta = true
+                TriggerEvent('esx_status:setDisplay', 0.0)
+            end
+            Citizen.Wait(5)
+        end
+        while kartta do
+            DisplayRadar(false)
+            if IsControlJustPressed(1, 56) then --F9
+                kartta = false
+                DisplayRadar(true)
+                TriggerEvent('esx_status:setDisplay', 1.0)
+            end
+            Citizen.Wait(5)
+        end
+    end
+end)
+-- BY CLOUDIS
+-- BY CLOUDIS
+-- BY CLOUDIS
+-- BY CLOUDIS
+-----------------------------CLOUDIS RADAR
